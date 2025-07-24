076f842b9fc7fcea57335d82174f77679a55ce19
Fedor Kostrov <kostrov.f.dev@gmail.com>
2025-07-24 12:09:14 +0300

    service interface id arg renaming

--- internal/service/service.go
+++ internal/service/service.go
@@ -8,35 +8,35 @@ import (
 
 type QuestionService interface {
 	Create(ctx context.Context, newQuestion *model.NewQuestion) (int, error)
-	Get(ctx context.Context, questionId int) (*model.Question, error)
-	Update(ctx context.Context, questionId int, updatedQuestion *model.UpdatedQuestion) error
-	Delete(ctx context.Context, questionId int) error
+	Get(ctx context.Context, id int) (*model.Question, error)
+	Update(ctx context.Context, id int, updatedQuestion *model.UpdatedQuestion) error
+	Delete(ctx context.Context, id int) error
 }
 
 type QuestionSetService interface {
 	Create(ctx context.Context, newQuestionSet *model.NewQuestionSet) (int, error)
-	Get(ctx context.Context, questionSetId int) (*model.QuestionSet, error)
-	Update(ctx context.Context, questionSetId int, updatedQuestionSet *model.UpdatedQuestionSet) error
-	Delete(ctx context.Context, questionSetId int) error
+	Get(ctx context.Context, id int) (*model.QuestionSet, error)
+	Update(ctx context.Context, id int, updatedQuestionSet *model.UpdatedQuestionSet) error
+	Delete(ctx context.Context, id int) error
 }
 
 type DomainService interface {
 	Create(ctx context.Context, newDomain *model.NewDomain) (int, error)
-	Get(ctx context.Context, domainId int) (*model.Domain, error)
-	Update(ctx context.Context, domainId int, updatedDomain *model.UpdatedDomain) error
-	Delete(ctx context.Context, domainId int) error
+	Get(ctx context.Context, id int) (*model.Domain, error)
+	Update(ctx context.Context, id int, updatedDomain *model.UpdatedDomain) error
+	Delete(ctx context.Context, id int) error
 }
 
 type CategoryService interface {
 	Create(ctx context.Context, newCategory *model.NewCategory) (int, error)
-	Get(ctx context.Context, categoryId int) (*model.Category, error)
-	Update(ctx context.Context, categoryId int, updatedCategory *model.UpdatedCategory) error
-	Delete(ctx context.Context, categoryId int) error
+	Get(ctx context.Context, id int) (*model.Category, error)
+	Update(ctx context.Context, id int, updatedCategory *model.UpdatedCategory) error
+	Delete(ctx context.Context, id int) error
 }
 
 type SubcategoryService interface {
 	Create(ctx context.Context, newSubcategory *model.NewSubcategory) (int, error)
-	Get(ctx context.Context, subcategoryId int) (*model.Subcategory, error)
-	Update(ctx context.Context, subcategoryId int, updatedSubcategory *model.UpdatedSubcategory) error
-	Delete(ctx context.Context, subcategoryId int) error
+	Get(ctx context.Context, id int) (*model.Subcategory, error)
+	Update(ctx context.Context, id int, updatedSubcategory *model.UpdatedSubcategory) error
+	Delete(ctx context.Context, id int) error
 }
