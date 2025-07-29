789f805c58620bba1ffad7465748fe6d9cd9e911
Fedor Kostrov <kostrov.f.dev@gmail.com>
2025-07-29 16:57:54 +0300

    question delete use case

--- internal/repository/question/pg/repository.go
+++ internal/repository/question/pg/repository.go
@@ -103,6 +103,12 @@ func (r *repo) Update(ctx context.Context, id int, updatedQuestion *model.Update
 }
 
 func (r *repo) Delete(ctx context.Context, id int) error {
+	query := `DELETE FROM question WHERE id = $1`
+	_, err := r.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
