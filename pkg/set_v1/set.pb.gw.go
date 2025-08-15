3fc949962c7bf735afe852f1c811bef81485d5c3
Fedor Kostrov <kostrov.f.dev@gmail.com>
2025-08-15 10:30:58 +0300

    set-search proto

--- pkg/set_v1/set.pb.gw.go
+++ pkg/set_v1/set.pb.gw.go
@@ -185,6 +185,41 @@ func local_request_SetV1_Delete_0(ctx context.Context, marshaler runtime.Marshal
 	return msg, metadata, err
 }
 
+var filter_SetV1_ListSearch_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+
+func request_SetV1_ListSearch_0(ctx context.Context, marshaler runtime.Marshaler, client SetV1Client, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq ListSearchRequest
+		metadata runtime.ServerMetadata
+	)
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_SetV1_ListSearch_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := client.ListSearch(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_SetV1_ListSearch_0(ctx context.Context, marshaler runtime.Marshaler, server SetV1Server, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq ListSearchRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_SetV1_ListSearch_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := server.ListSearch(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 // RegisterSetV1HandlerServer registers the http handlers for service SetV1 to "mux".
 // UnaryRPC     :call SetV1Server directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -270,6 +305,26 @@ func RegisterSetV1HandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 		forward_SetV1_Delete_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
+	mux.Handle(http.MethodGet, pattern_SetV1_ListSearch_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/set_v1.SetV1/ListSearch", runtime.WithHTTPPathPattern("/api/v1/set/search"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_SetV1_ListSearch_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_SetV1_ListSearch_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -379,19 +434,38 @@ func RegisterSetV1HandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 		}
 		forward_SetV1_Delete_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodGet, pattern_SetV1_ListSearch_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/set_v1.SetV1/ListSearch", runtime.WithHTTPPathPattern("/api/v1/set/search"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SetV1_ListSearch_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_SetV1_ListSearch_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	return nil
 }
 
 var (
-	pattern_SetV1_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "set"}, ""))
-	pattern_SetV1_Get_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
-	pattern_SetV1_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
-	pattern_SetV1_Delete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
+	pattern_SetV1_Create_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "set"}, ""))
+	pattern_SetV1_Get_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
+	pattern_SetV1_Update_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
+	pattern_SetV1_Delete_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "set", "id"}, ""))
+	pattern_SetV1_ListSearch_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "set", "search"}, ""))
 )
 
 var (
-	forward_SetV1_Create_0 = runtime.ForwardResponseMessage
-	forward_SetV1_Get_0    = runtime.ForwardResponseMessage
-	forward_SetV1_Update_0 = runtime.ForwardResponseMessage
-	forward_SetV1_Delete_0 = runtime.ForwardResponseMessage
+	forward_SetV1_Create_0     = runtime.ForwardResponseMessage
+	forward_SetV1_Get_0        = runtime.ForwardResponseMessage
+	forward_SetV1_Update_0     = runtime.ForwardResponseMessage
+	forward_SetV1_Delete_0     = runtime.ForwardResponseMessage
+	forward_SetV1_ListSearch_0 = runtime.ForwardResponseMessage
 )
